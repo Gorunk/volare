c55511c3a27a5de06e2b2e829952aa6d354007a9
José Padilla <40579916+JosePadilla98@users.noreply.github.com>
2022-11-22 11:19:54 +0100

    Commit de antes de meter plugins por si se lia

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,5 +3,23 @@
 @section('title', 'DashBoard')
 
 @section('content')
-    <h1>DASHBOARD</h1>
+    <h1>DASHBOARD</h1><br><br>
+    <div class="dashgrid">
+        <fieldset class="blancoFondo">
+            <h2>Últimas compras</h2>
+            <table class="dashCompras">
+                <tr class="blancoTexto azul">
+                    <th>USUARIO</th>
+                    <th>VUELO</th>
+                    <th>RESERVA</th>
+                    <th>DESTINO</th>
+                    <th>ORIGEN</th>
+                    <th>ACCIONES</th>
+                </tr>
+            </table>
+        </fieldset>
+        <fieldset class="blancoFondo">
+            <h2>Vuelos con plazas vacantes</h2>
+        </fieldset>
+    </div>
 @endsection
