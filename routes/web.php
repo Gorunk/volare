458ba76983bd7dda4a6393416238848f87c68f27
José Padilla <40579916+JosePadilla98@users.noreply.github.com>
2023-01-17 13:32:39 +0100

    Ahora si estan los request, actualizacion buena

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,21 +18,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/login', [UserController::class, 'login'])->name('login');
+
+Route::post('/dashboard', [UserController::class, 'index'])->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-});
+})->name('dashboard');
 
 Route::get('/vuelos', function () {
     return view('vuelos');
-});
+})->name('vuelos');
 
 Route::get('/ventas', function () {
     return view('ventas');
-});
+})->name('ventas');
 
 Route::get('/users', function () {
     return view('users');
